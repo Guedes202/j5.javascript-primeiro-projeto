9573b11f31db406acef6f22d9a97801a2a346402
Guedes202 <00001105844924sp@al.educacao.sp.gov.br>
2024-06-14 08:30:32 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(400, 400);
+    background("blue");
+  }
+  
+  function draw() {
+    
+    stroke("green");
+    fill("orange");
+    
+    // console.log(mouseIsPressed);
+    
+    if (mouseIsPressed){
+          rect(mouseX,mouseY,50,50);
+    }
+    
+  }
+  
+  
